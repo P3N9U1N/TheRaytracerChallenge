96e1b43304618e11c0829be51b8dd185b79aa705
Thomas Heizmann <ThomasHeizmann@hotmail.com>
2021-07-15 17:42:35 +0200

    Chapter 1

---
+++ src/tuple.ts
@@ -0,0 +1,65 @@
+export class Tuple {
+    public x: number;
+    public y: number;
+    public z: number;
+    public w: number;
+
+    private static EPSILON: number = 0.00001;
+
+    constructor(x: number, y: number, z: number, w: number) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+    public isPoint(): boolean {
+        return this.w == 1;
+    }
+    public isVector(): boolean {
+        return this.w == 0;
+    }
+
+    public add(tuple: Tuple): Tuple {
+        return new Tuple(this.x + tuple.x, this.y + tuple.y, this.z + tuple.z, this.w + tuple.w)
+    }
+    public multiply(scalar: number): Tuple {
+        return new Tuple(this.x * scalar, this.y * scalar, this.z * scalar, this.w * scalar)
+    }
+    public divide(scalar: number): Tuple {
+        return new Tuple(this.x / scalar, this.y / scalar, this.z / scalar, this.w / scalar)
+    }
+    public substract(tuple: Tuple): Tuple {
+        return new Tuple(this.x - tuple.x, this.y - tuple.y, this.z - tuple.z, this.w - tuple.w)
+    }
+    public negate(): Tuple {
+        return new Tuple(-this.x, -this.y, -this.z, -this.w)
+    }
+    public normalize(): Tuple {
+        return this.divide(this.magnitude());
+    }
+    public magnitude() {
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z + this.w * this.w);
+    }
+    public dot(tuple: Tuple): number {
+        return this.x * tuple.x + this.y * tuple.y + this.z * tuple.z + this.w * tuple.w;
+    }
+    public cross(tuple: Tuple): Tuple {
+        return Tuple.vector(this.y * tuple.z - this.z * tuple.y,
+            this.z * tuple.x - this.x * tuple.z,
+            this.x * tuple.y - this.y * tuple.x
+        );
+    }
+
+    public equals(tuple: Tuple) {
+        return Math.abs(this.x - tuple.x) < Tuple.EPSILON
+            && Math.abs(this.y - tuple.y) < Tuple.EPSILON
+            && Math.abs(this.z - tuple.z) < Tuple.EPSILON;
+    }
+    static point(x: number, y: number, z: number): Tuple {
+        return new Tuple(x, y, z, 1);
+    }
+    static vector(x: number, y: number, z: number): Tuple {
+        return new Tuple(x, y, z, 0);
+    }
+
+}
---
+++ test/tuple.test.ts
@@ -0,0 +1,79 @@
+import { Tuple } from "../src/tuple"
+
+
+describe('tuple with w==0', () => {
+    var tuple = new Tuple(1, 1, 1, 0);
+    it('should be a vector', () => {
+        expect(tuple.isVector()).toBeTruthy();
+    })
+    it('should not be a point', () => {
+        expect(tuple.isPoint()).toBeFalsy();
+    })
+}
+);
+describe('tuple with w==1', () => {
+    var tuple = new Tuple(1, 1, 1, 1);
+    it('should not be a vector', () => {
+        expect(tuple.isVector()).toBeFalsy();
+    })
+    it('should be a point', () => {
+        expect(tuple.isPoint()).toBeTruthy();
+    })
+}
+);
+test("Constructor", () => {
+    expect(new Tuple(4.3, -4.2, 3.1, 0)).toEqual({ x: 4.3, y: -4.2, z: 3.1, w: 0 });
+}
+);
+test("point() creates tuples with w==1", () => {
+    expect(Tuple.point(1, 1, 1).w).toBe(1);
+}
+);
+test("vector() creates tuples with w==0", () => {
+    expect(Tuple.vector(1, 1, 1).w).toBe(0);
+}
+);
+test("add 2 points", () => {
+    expect((new Tuple(3, -2, 5, 1).add(new Tuple(-2, 3, 1, 0)))).toEqual(new Tuple(1, 1, 6, 1));
+}
+);
+test("substract 2 points", () => {
+    expect(Tuple.point(3, 2, 1).substract(Tuple.point(5, 6, 7))).toEqual(Tuple.vector(-2, -4, -6));
+}
+);
+test("substract a vector from a point", () => {
+    expect(Tuple.point(3, 2, 1).substract(Tuple.vector(5, 6, 7))).toEqual(Tuple.point(-2, -4, -6));
+}
+);
+test("negate", () => {
+    expect(new Tuple(1, -2, 3, -4).negate()).toEqual(new Tuple(-1, 2, -3, 4));
+}
+);
+test("multiply", () => {
+    expect(new Tuple(1, -2, 3, -4).multiply(3.5)).toEqual(new Tuple(3.5, -7, 10.5, -14));
+}
+);
+test("divide", () => {
+    expect(new Tuple(1, -2, 3, -4).divide(2)).toEqual(new Tuple(0.5, -1, 1.5, -2));
+}
+);
+test("magnitude", () => {
+    expect(Tuple.vector(1, 2, 3).magnitude()).toBeCloseTo(Math.sqrt(14));
+}
+);
+
+test("normalize", () => {
+    expect(Tuple.vector(1, 2, 3).normalize().equals(Tuple.vector(0.26726, 0.53452, 0.80178))).toBeTruthy();
+}
+);
+test("dot product", () => {
+    expect(Tuple.vector(1, 2, 3).dot(Tuple.vector(2, 3, 4))).toBe(20);
+}
+);
+test("cross product", () => {
+    var a = Tuple.vector(1, 2, 3);
+    var b = Tuple.vector(2, 3, 4);
+    expect(a.cross(b)).toEqual(Tuple.vector(-1, 2, -1));
+    expect(b.cross(a)).toEqual(Tuple.vector(1, -2, 1));
+}
+);
