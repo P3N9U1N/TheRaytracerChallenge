fda0cb19172444ca4cf95b82c82d3aa04a6863ba
Thomas Heizmann <ThomasHeizmann@hotmail.com>
2021-08-26 16:13:56 +0200

    Add webpack typescript config

--- raytracer-gui/webpack.config.js
+++ raytracer-gui/webpack.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
+//const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  entry: '../node_modules/raytracer-gui/dist/index.js',
+  entry: './src/index.ts',
   mode: 'development',
+  devtool: 'inline-source-map',
+  "context": __dirname,
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          "loader": "ts-loader",
+          "options": {
+            "projectReferences": true,
+            "transpileOnly": false
+          }
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  }, 
+  resolve: {
+    extensions: ['.tsx', '.ts', 'js'],
+    alias: {
+      raytracer: path.resolve(__dirname, '../raytracer/src/')   
+    },
+  },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, ''),
+    path: path.resolve(__dirname, 'dist'),
   },
 };
--- spikes/chapter6/test.ts
+++ spikes/chapter6/test.ts
@@ -6,7 +6,6 @@ import * as fs from "fs";
 import { Ray } from "raytracer/ray";
 import { Material } from "raytracer/material";
 import { PointLight } from "raytracer/pointLight";
-import { Intersections } from "raytracer/intersection";
 
 
 
@@ -51,45 +50,7 @@ for (var y=0;y<c.height;y++)
 
 
 
-function simpleRenderTest():Canvas
-{
-    var c = new Canvas(1024,1024);
-    var rayOrigin = Tuple.point(0,0,-5);
-    var wallz=10;
-    var wallSize=7;
-    var pixelSize=wallSize/ c.height;
-    var half=wallSize/2;
-    var shape=new Sphere(1)
-    shape.material= new Material();
-    shape.material.color=new Color(1,0.2,1);
-    var light = new PointLight(Tuple.point(-10,10,-10),Color.WHITE.clone());
-    var xs= new Intersections(10);
-    for (var y=0;y<c.height;y++)
-    {
-        var worldY=half-pixelSize*y;
-        for (var x=0;x<c.width;x++)
-        {
-        
-            var worldX=-half+pixelSize*x;
-            var position = Tuple.point(worldX,worldY,wallz);
-            var r= new Ray(rayOrigin, position.substract(rayOrigin).normalize() );
-            shape.intersect(r,xs);
-            if (xs.length>0)
-            {
-                var hit= xs.hit();
-                var point=r.position(hit.t);
-                var normal=shape.normalAt(point)
-                var color=shape.material.lighting(light,point, r.direction.negate() ,normal);
-                c.writePixel(x,y,color);
-            }
-            xs.clear();
-
-        }
-    }
-    return c;
-}
 
 
 
-
-fs.writeFileSync("picture.ppm" ,simpleRenderTest().toPpm());
+fs.writeFileSync("picture.ppm" ,c.toPpm());
